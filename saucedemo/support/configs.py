7416f38a765af0f086436e36e11d895dc25366d6
Qamar-Abbas-14 <qamar.abbas14@hotmail.com>
2022-07-12 00:11:37 +0500

    Linux Support

--- saucedemo/support/configs.py
+++ saucedemo/support/configs.py
@@ -6,17 +6,22 @@ from selenium.webdriver.firefox.service import Service
 from pathlib import Path
 
 PROJECT_DIR = Path(__file__).parents[1]
-CHROME_DRIVER_PATH = f"{PROJECT_DIR}\\resources\\chromedriver.exe"
-FIREFOX_DRIVER_PATH = f"{PROJECT_DIR}\\resources\\geckodriver.exe"
+OS_NAME = os.name
+
+if OS_NAME == 'nt':
+    CHROME_DRIVER_PATH = f"{PROJECT_DIR}\\resources\\windows_driver\\chromedriver.exe"
+    FIREFOX_DRIVER_PATH = f"{PROJECT_DIR}\\resources\\windows_driver\\geckodriver.exe"
+else:
+    FIREFOX_DRIVER_PATH = f"{PROJECT_DIR}/resources/linux_driver/geckodriver"
+    CHROME_DRIVER_PATH = f"{PROJECT_DIR}/resources/linux_driver/chromedriver"
 
 def browser(brower_type):
     if brower_type == "Chrome":
-        driver = webdriver.Chrome(CHROME_DRIVER_PATH)
+        driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
         driver.maximize_window()
         driver.implicitly_wait(5)
     if brower_type == "Firefox":
-        firefox_service = Service(FIREFOX_DRIVER_PATH)
-        driver = webdriver.Firefox(service=firefox_service)
+        driver = webdriver.Firefox(executable_path=FIREFOX_DRIVER_PATH)
         driver.maximize_window()
         driver.implicitly_wait(5)
     return driver
